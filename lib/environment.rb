aed9e08efe1505b86f4098c18080bb2eb6e0426d
Dan Suarez <suarez.daniel101@gmail.com>
2020-02-26 23:16:49 +0000

    new require additions made to environment file

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,5 +1,5 @@
-require "happy_plastics/version"
-require "happy_plastics/cli"
+require_relative "./happy_plastics/version"
+require_relative "./happy_plastics/cli"
 
 module HappyPlastics
   class Error < StandardError; end
