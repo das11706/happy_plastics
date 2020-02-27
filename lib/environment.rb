99a97dbdb6d23241a1b25d88c35dfeb680192231
Dan Suarez <suarez.daniel101@gmail.com>
2020-02-27 02:59:40 +0000

    changed to require_relative for pry in environment file

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,7 +1,7 @@
 require_relative "./happy_plastics/version"
 require_relative "./happy_plastics/cli"
 
-require "pry"
+require_relative "pry"
 
 module HappyPlastics
   class Error < StandardError; end
