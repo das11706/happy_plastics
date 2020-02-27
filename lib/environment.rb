1b4aa43114a96b4e442900e1b06b6e0b30707f76
Dan Suarez <suarez.daniel101@gmail.com>
2020-02-27 03:04:12 +0000

    change pry back to require in environment file

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,7 +1,7 @@
 require_relative "./happy_plastics/version"
 require_relative "./happy_plastics/cli"
 
-require_relative "pry"
+require "pry"
 
 module HappyPlastics
   class Error < StandardError; end
