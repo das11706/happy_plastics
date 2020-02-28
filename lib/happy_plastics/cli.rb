c8693000aa23d6076187e8371159b6977929f6be
Dan Suarez <suarez.daniel101@gmail.com>
2020-02-28 04:11:41 +0000

    stubbing out CLI class

--- lib/happy_plastics/cli.rb
+++ lib/happy_plastics/cli.rb
@@ -1,6 +1,7 @@
 class HappyPlastics::CLI 
   def call
     puts "Welcome to HappyPlastics!"
+    puts "Our aim is to make a greener world by informing you on the variety of available plastics, their uses, and their recycling potential."
     puts "Please select a plastic symbol number for more information."
     get_listed_plastics
     get_user_plastic
