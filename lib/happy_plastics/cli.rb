03dc50aac77195ecc809ed8866f039d231e5e7ca
Dan Suarez <suarez.daniel101@gmail.com>
2020-02-28 00:05:15 +0000

    stubbing out the CLI class

--- lib/happy_plastics/cli.rb
+++ lib/happy_plastics/cli.rb
@@ -1,6 +1,12 @@
 class HappyPlastics::CLI 
   def call
     puts "Welcome to HappyPlastics!"
-    # binding.pry 
+    get_listed_user_plastic_numbers
+    # list_plastic_name
   end
+  
+  def get_listed_user_plastic_numbers
+    
+  end
+
 end 
