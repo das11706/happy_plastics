6b3ce3dd8f61e3c9c28a98e408e37fbcfb8c690c
Dan Suarez <suarez.daniel101@gmail.com>
2020-02-28 04:41:53 +0000

    stubbing out CLI class

--- lib/happy_plastics/cli.rb
+++ lib/happy_plastics/cli.rb
@@ -1,11 +1,11 @@
 class HappyPlastics::CLI 
   def call
-    puts "Welcome to HappyPlastics!"
-    puts "Our aim is to make a greener world by informing you on the variety of available plastics, their uses, and their recycling potential."
-    puts "Please select a plastic symbol number for more information."
+    puts "\nWelcome to HappyPlastics!\n"
+    puts "\nOur aim is to make a greener world by informing you of the variety of available plastics, their uses, and their recycling potential.\n"
+    puts "\nPlease select a plastic symbol number for more information.\n"
     get_listed_plastics
+    list_plastics
     get_user_plastic
-    # list_plastic_name
   end
   
   def get_listed_plastics
@@ -13,10 +13,13 @@ class HappyPlastics::CLI
     @plastics = ["1", "2", "3", "4", "5", "6", "7"]
   end
 
-  def get_user_plastic
-    # list plastics
+  def list_plastics
     @plastics.each do |month|
       puts "Symbol ##{month}"
     end
+  end
+  
+  def get_user_plastic
+    
   end
 end 
