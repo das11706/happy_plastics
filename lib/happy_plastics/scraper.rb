096d41e9c01a5114d99736e5e302e4ac22475634
Dan Suarez <suarez.daniel101@gmail.com>
2020-03-10 20:43:03 +0000

    expanding scraper class

--- lib/happy_plastics/scraper.rb
+++ lib/happy_plastics/scraper.rb
@@ -1,3 +1,6 @@
 class HappyPlastics::Scraper
-     
+    def scrape_plastics
+      doc = Nokogiri::HTML(open(https://www.goodhousekeeping.com/home/g804/recycling-symbols-plastics-460321/))
+      binding.pry
+    end
 end
