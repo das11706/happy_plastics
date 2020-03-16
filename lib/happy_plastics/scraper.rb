93d9bcce3b29a222a8d39fff318e01b388ce60aa
Dan Suarez <suarez.daniel101@gmail.com>
2020-03-16 17:26:02 +0000

    expanding Scraper class

--- lib/happy_plastics/scraper.rb
+++ lib/happy_plastics/scraper.rb
@@ -1,6 +1,11 @@
 class HappyPlastics::Scraper
     def self.scrape_plastics
       doc = Nokogiri::HTML(open("https://www.goodhousekeeping.com/home/g804/recycling-symbols-plastics-460321/"))
+      # plastics = doc.css("div.listicle-body-content picture")
+      plastics = doc.css("div.listicle-slide-hed")
+        plastics.each do |p|
+          name = p.text
+        end
       binding.pry
     end
 end
