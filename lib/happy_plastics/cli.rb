7acacc76ce819cfd71f2dc8489b5b5ab802e952a
Dan Suarez <suarez.daniel101@gmail.com>
2020-06-10 20:37:32 +0000

    adjusted show_facts_for method

--- lib/happy_plastics/cli.rb
+++ lib/happy_plastics/cli.rb
@@ -46,7 +46,7 @@ class HappyPlastics::CLI
   def show_facts_for(chosen_plastic)
     plastic = @plastics[chosen_plastic - 1]
     plastic.get_facts
-    puts "Here is the name for #{plastic.num}:"
+    puts "Here is the name for symbol ##{plastic.num}:"
     # binding.pry
     HappyPlastics::Fact.all.each.with_index(1) do |n|
       puts n.name
