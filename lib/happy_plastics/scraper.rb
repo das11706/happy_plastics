a05e8e2f2eff6dcc6bfd4145dca6b1f8673aa32d
Dan Suarez <suarez.daniel101@gmail.com>
2020-06-17 20:45:32 +0000

    made changes to scrape_facts method

--- lib/happy_plastics/scraper.rb
+++ lib/happy_plastics/scraper.rb
@@ -13,11 +13,15 @@ class HappyPlastics::Scraper
   def self.scrape_facts(plastic)
     doc = Nokogiri::HTML(open("https://www.goodhousekeeping.com/home/g804/recycling-symbols-plastics-460321/"))
     # plastics = doc.css("div.listicle-body-content picture")
+    
     plastic_name = doc.css("span.listicle-slide-hed-text")
       plastic_name.each do |n|
         name = n.text
         HappyPlastics::Fact.new(name, plastic)
         # HappyPlastics::Fact.new("HDPE", plastic)
       end
+      
+    plastic_found_in = doc.css("span.listicle-slide-")
+      # binding.pry
   end
 end
