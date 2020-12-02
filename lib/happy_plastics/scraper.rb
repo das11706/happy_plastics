7ea471570ca0ad3f54075ec41dc79554effa6911
Dan Suarez <suarez.daniel101@gmail.com>
2020-12-02 02:51:59 +0000

    Done.

--- lib/happy_plastics/scraper.rb
+++ lib/happy_plastics/scraper.rb
@@ -17,19 +17,19 @@ class HappyPlastics::Scraper
     # binding.pry 
   end 
   
-  def self.scrape_fact(plastic)
-    doc = Nokogiri::HTML(open("https://www.goodhousekeeping.com/home/g804/recycling-symbols-plastics-460321/"))
-    plastic_name = doc.css("div.listicle-body-content div")
-    # plastic_name = doc.css("span.listicle-slide-hed-text")
-      plastic_name.each do |n|
-        name = n.css("span.listicle-slide-hed-text").text
-        HappyPlastics::Plastic.new(name)
-        binding.pry
-        # HappyPlastics::Fact.new(name, plastic)
-        # HappyPlastics::Fact.new("HDPE", plastic)
-      end
+  # def self.scrape_fact(plastic)
+  #   doc = Nokogiri::HTML(open("https://www.goodhousekeeping.com/home/g804/recycling-symbols-plastics-460321/"))
+  #   plastic_name = doc.css("div.listicle-body-content div")
+  #   # plastic_name = doc.css("span.listicle-slide-hed-text")
+  #     plastic_name.each do |n|
+  #       name = n.css("span.listicle-slide-hed-text").text
+  #       HappyPlastics::Plastic.new(name)
+  #       binding.pry
+  #       # HappyPlastics::Fact.new(name, plastic)
+  #       # HappyPlastics::Fact.new("HDPE", plastic)
+  #     end
       
-    # plastic_found_in = doc.css("span.listicle-slide-")
-      # binding.pry
-  end
+  #   # plastic_found_in = doc.css("span.listicle-slide-")
+  #     # binding.pry
+  # end
 end
