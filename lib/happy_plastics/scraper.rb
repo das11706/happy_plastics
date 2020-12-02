776527d97c187b8f555afb98dfadaf2735165585
Dan Suarez <suarez.daniel101@gmail.com>
2020-12-02 02:23:46 +0000

    Done.

--- lib/happy_plastics/scraper.rb
+++ lib/happy_plastics/scraper.rb
@@ -1,13 +1,19 @@
 class HappyPlastics::Scraper 
-  def self.scrape_plastic_nums
-    doc = Nokogiri::HTML(open("https://www.goodhousekeeping.com/home/g804/recycling-symbols-plastics-460321/"))
+  def self.scrape_plastic_name
+    # doc = Nokogiri::HTML(open("https://www.goodhousekeeping.com/home/g804/recycling-symbols-plastics-460321/"))
+     doc = Nokogiri::HTML(open("https://www.goodhousekeeping.com/home/g804/recycling-symbols-plastics-460321/"))
     # plastics = doc.css("div.listicle-body-content picture")
     # plastic_facts = doc.css("div.listicle-body-content")
-    plastics = doc.css("span.listicle-slide-hed-number")
-      plastics.each do |p|
-        num = p.text 
-        HappyPlastics::Plastic.new(num) 
-      end 
+      plastic_name = doc.css("div.listicle-body-content div")
+    # plastics = doc.css("span.listicle-slide-hed-number")
+      plastic_name.each do |n|
+        name = n.css("span.listicle-slide-hed-text").text
+        HappyPlastics::Plastic.new(name)
+      end
+      # plastics.each do |p|
+      #   num = p.text 
+      #   HappyPlastics::Plastic.new(num) 
+      # end 
     # binding.pry 
   end 
   
