816466052d53d68a075bb94bb4c5b4b4bb6f0977
Dan Suarez <suarez.daniel101@gmail.com>
2021-03-10 04:12:51 +0000

    amended variable names in self.scrape_plastic method

--- lib/happy_plastics/scraper.rb
+++ lib/happy_plastics/scraper.rb
@@ -1,9 +1,9 @@
 class HappyPlastics::Scraper 
   def self.scrape_plastic
     doc = Nokogiri::HTML(open("https://www.goodhousekeeping.com/home/g804/recycling-symbols-plastics-460321/"))
-    @plastic_nums = doc.css("span.listicle-slide-hed-number")
-    @plastic_names = doc.css("span.listicle-slide-hed-text")
-    @plastic_nums.zip(@plastic_names).each do |num, name|
+    plastic_nums = doc.css("span.listicle-slide-hed-number")
+    plastic_names = doc.css("span.listicle-slide-hed-text")
+    plastic_nums.zip(plastic_names).each do |num, name|
        number = num.text 
        name = name.text
        HappyPlastics::Plastic.new(number, name)
